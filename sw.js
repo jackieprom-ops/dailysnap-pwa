92f1c2d31e2f27759294a1d228ad883c3b08f0b7
jackieprom-ops <jackieprom@gmail.com>
2025-11-30 20:40:17 +0700

    Implement service worker with caching strategies

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-// service worker placeholder
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('dailysnap-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './app.js',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
